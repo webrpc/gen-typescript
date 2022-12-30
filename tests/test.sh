8042869f8e4495cb071c5e163c53b21392560a77
Vojtech Vitek <vojtech.vitek@golang.cz>
2022-12-31 00:10:18 +0100

    Update to webrpc@v0.10.0 test suite

---
+++ tests/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PORT=9988
+
+webrpc-test -version
+webrpc-test -print-schema > ./test.ridl
+webrpc-gen -schema=./test.ridl -target=typescript -client -out=./client.ts
+
+webrpc-test -server -port=$PORT -timeout=5s &
+
+# Wait until http://localhost:$PORT is available, up to 10s.
+for (( i=0; i<100; i++ )); do nc -z localhost $PORT && break || sleep 0.1; done
+
+npm run test
